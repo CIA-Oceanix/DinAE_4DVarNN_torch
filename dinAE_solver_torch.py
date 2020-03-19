1008e4bef6b40e981d957844f151f19ce0e857a9
ronan fablet <rfablet@ronans-MacBook-Pro-341.local>
2020-03-19 23:13:25 +0100

    add file

--- dinAE_solver_torch.py
+++ dinAE_solver_torch.py
@@ -91,6 +91,53 @@ class ConvLSTM2d(torch.nn.Module):
 
         return hidden, cell
 
+class ConvLSTM1d(torch.nn.Module):
+    def __init__(self, input_size, hidden_size, kernel_size = 3):
+        super(ConvLSTM2d, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.kernel_size = kernel_size
+        self.padding = int((kernel_size - 1) / 2)
+        self.Gates = torch.nn.Conv1d(input_size + hidden_size, 4 * hidden_size, kernel_size = self.kernel_size, stride = 1, padding = self.padding)
+
+    def forward(self, input_, prev_state):
+
+        # get batch and spatial sizes
+        batch_size = input_.shape[0]
+        spatial_size = input_.shape[2:]
+
+        # generate empty prev_state, if None is provided
+        if prev_state is None:
+            state_size = [batch_size, self.hidden_size] + list(spatial_size)
+            prev_state = (
+                torch.autograd.Variable(torch.zeros(state_size)).to(device),
+                torch.autograd.Variable(torch.zeros(state_size)).to(device)
+            )
+
+        # prev_state has two components
+        prev_hidden, prev_cell = prev_state
+
+        # data size is [batch, channel, height, width]
+        stacked_inputs = torch.cat((input_, prev_hidden), 1)
+        gates = self.Gates(stacked_inputs)
+
+        # chunk across channel dimension: split it to 4 samples at dimension 1
+        in_gate, remember_gate, out_gate, cell_gate = gates.chunk(4, 1)
+
+        # apply sigmoid non linearity
+        in_gate = torch.sigmoid(in_gate)
+        remember_gate = torch.sigmoid(remember_gate)
+        out_gate = torch.sigmoid(out_gate)
+
+        # apply tanh non linearity
+        cell_gate = torch.tanh(cell_gate)
+
+        # compute current cell and hidden state
+        cell = (remember_gate * prev_cell) + (in_gate * cell_gate)
+        hidden = out_gate * torch.tanh(cell)
+
+        return hidden, cell
+
 #if torch.cuda.is_available():
 #  torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
@@ -125,15 +172,27 @@ class Model_AE_GradFP(torch.nn.Module):
         self.NProjFP   = int(NiterProjection)
         self.NGrad     = int(NiterGrad)
         self.shape     = ShapeData
-        if self.OptimType == 0: # fixed-step gradient descent
-          self.conv1    = torch.nn.Conv2d(self.shape[0], self.shape[0], (1,1), padding=0)
-        elif self.OptimType == 1: # ConvNet gradient descent using previous and current gradient
-          self.conv1    = torch.nn.Conv2d(2*self.shape[0], 8*self.shape[0], (3,3), padding=1)
-          self.conv2    = torch.nn.Conv2d(8*self.shape[0], 16*self.shape[0], (3,3), padding=1)
-          self.conv3    = torch.nn.Conv2d(16*self.shape[0], self.shape[0], (3,3), padding=1)
-        elif self.OptimType == 2: # ConvNet gradient descent using previous and current gradient
-          self.lstm1    = ConvLSTM2d(self.shape[0],5*self.shape[0],3)
-          self.conv1    = torch.nn.Conv2d(5*self.shape[0], self.shape[0], (1,1), padding=0)
+        
+        if len(self.shape) == 2: ## 1D Data
+            if self.OptimType == 0: # fixed-step gradient descent
+              self.conv1    = torch.nn.Conv1d(self.shape[0], self.shape[0],1, padding=0)              
+            elif self.OptimType == 1: # ConvNet gradient descent using previous and current gradient
+              self.conv1    = torch.nn.Conv1d(2*self.shape[0], 8*self.shape[0],3, padding=1)
+              self.conv2    = torch.nn.Conv1d(8*self.shape[0], 16*self.shape[0],3, padding=1)
+              self.conv3    = torch.nn.Conv1d(16*self.shape[0], self.shape[0],3, padding=1)
+            elif self.OptimType == 2: # ConvNet gradient descent using previous and current gradient
+              self.lstm1    = ConvLSTM1d(self.shape[0],5*self.shape[0],3)
+              self.conv1    = torch.nn.Conv1d(5*self.shape[0], self.shape[0], 1, padding=0)           
+        elif len(self.shape) == 3: ## 2D Data            
+            if self.OptimType == 0: # fixed-step gradient descent
+              self.conv1    = torch.nn.Conv2d(self.shape[0], self.shape[0], (1,1), padding=0)
+            elif self.OptimType == 1: # ConvNet gradient descent using previous and current gradient
+              self.conv1    = torch.nn.Conv2d(2*self.shape[0], 8*self.shape[0], (3,3), padding=1)
+              self.conv2    = torch.nn.Conv2d(8*self.shape[0], 16*self.shape[0], (3,3), padding=1)
+              self.conv3    = torch.nn.Conv2d(16*self.shape[0], self.shape[0], (3,3), padding=1)
+            elif self.OptimType == 2: # ConvNet gradient descent using previous and current gradient
+              self.lstm1    = ConvLSTM2d(self.shape[0],5*self.shape[0],3)
+              self.conv1    = torch.nn.Conv2d(5*self.shape[0], self.shape[0], (1,1), padding=0)
     def forward(self, x_inp,mask):
         #mask   = torch.add(1.0,torch.mul(mask,0.0)) # set mask to 1 # debug
         mask_  = torch.add(1.0,torch.mul(mask,-1.0)) #1. - mask
