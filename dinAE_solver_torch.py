92c3e544fc7876bf770c36b7c22655e1192d2b10
ronan fablet <rfablet@ronans-MacBook-Pro-341.local>
2020-03-21 10:07:30 +0100

    add file

--- dinAE_solver_torch.py
+++ dinAE_solver_torch.py
@@ -12,6 +12,26 @@ import torch.nn.functional as F
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# Constrained Conv1D Layer with zero-weight at central point
+class ConstrainedConv1d(torch.nn.Conv1d):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride=1,
+                 padding=0,
+                 dilation=1,
+                 groups=1,
+                 bias=True):
+        super(ConstrainedConv1d,
+              self).__init__(in_channels, out_channels, kernel_size, stride,
+                             padding, dilation, groups, bias)
+        with torch.no_grad():
+          self.weight[:,:,int(self.weight.size(2)/2)+1] = 0.0
+    def forward(self, input):
+        return torch.nn.functional.conv1d(input, self.weight, self.bias, self.stride,
+                        self.padding, self.dilation, self.groups)
+
 # Constrained Conv2D Layer with zero-weight at central point
 class ConstrainedConv2d(torch.nn.Conv2d):
     def __init__(self,
@@ -188,11 +208,12 @@ class Model_AE_GradFP(torch.nn.Module):
         super(Model_AE_GradFP, self).__init__()
         self.model_AE = mod_AE
 
-        self.GradType = GradType
-        self.OptimType = OptimType
-        self.NProjFP   = int(NiterProjection)
-        self.NGrad     = int(NiterGrad)
-        self.shape     = ShapeData
+        with torch.no_grad():
+            self.GradType = GradType
+            self.OptimType = OptimType
+            self.NProjFP   = int(NiterProjection)
+            self.NGrad     = int(NiterGrad)
+            self.shape     = ShapeData
         
         if len(self.shape) == 2: ## 1D Data
             if self.OptimType == 0: # fixed-step gradient descent
