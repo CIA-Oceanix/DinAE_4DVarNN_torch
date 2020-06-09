9a5e4ca5471d3321fac85cd50a092de663d0ec1f
ronan fablet <rfablet@ronans-MacBook-Pro-341.local>
2020-06-09 15:53:29 +0200

    update

--- torch_4DVarNN_dinAE.py
+++ torch_4DVarNN_dinAE.py
@@ -190,17 +190,25 @@ class Compute_Grad(torch.nn.Module):
         
         self.alphaObs    = torch.nn.Parameter(torch.Tensor([1.]))
         self.alphaAE     = torch.nn.Parameter(torch.Tensor([1.]))
+        if( self.GradType == 3 ):
+            self.NbEngTerms    = 3            
+            self.alphaEngTerms = torch.nn.Parameter(torch.Tensor(np.ones((self.NbEngTerms,1))))
+            self.alphaEngTerms.requires_grad = True
+
+        if( self.GradType == 2 ):
+            self.alphaL1    = torch.nn.Parameter(torch.Tensor([1.]))
+            self.alphaL2     = torch.nn.Parameter(torch.Tensor([1.]))
 
     def forward(self, x,xpred,xobs,mask):
 
         # compute gradient
-        if self.GradType == 0: ## subgradient
+        if self.GradType == 0: ## subgradient for prior ||x-g(x)||^2 
           grad  = torch.add(xpred,-1.,x)
           grad2 = torch.add(x,-1.,xobs)
           grad  = torch.add(grad,1.,grad2)
           grad  = self.alphaAE * grad + self.alphaObs * grad2
 
-        else: ## true gradient using autograd
+        elif self.GradType == 1: ## true gradient using autograd for prior ||x-g(x)||^2 
           loss1 = torch.mean( (xpred - x)**2 )
           loss2 = torch.sum( (xobs - x)**2 * mask ) / torch.sum( mask )
           loss  = self.alphaAE**2 * loss1 + self.alphaObs**2 * loss2
@@ -209,6 +217,44 @@ class Compute_Grad(torch.nn.Module):
           grad = torch.autograd.grad(loss,x,create_graph=True)[0]
           #grad = torch.autograd.grad(loss,x)[0]
           
+        elif self.GradType == 2: ## true gradient using autograd for prior ||x-g(x)||
+          loss1 = self.alphaL2**2 * torch.mean( (xpred - x)**2 ) + self.alphaL1**2 * torch.mean( torch.abs(xpred - x) )
+          loss2 = torch.sum( (xobs - x)**2 * mask ) / torch.sum( mask )
+          loss  = self.alphaAE**2 * loss1 + self.alphaObs**2 * loss2
+          #=loss  = loss / ( self.alphaAE**2 + self.alphaObs**2)
+
+          grad = torch.autograd.grad(loss,x,create_graph=True)[0]
+          #grad = torch.autograd.grad(loss,x)[0]
+
+        elif self.GradType == 4: ## true gradient using autograd for prior ||g(x)||^2 
+          loss1 = torch.mean( xpred **2 )
+          loss2 = torch.sum( (xobs - x)**2 * mask ) / torch.sum( mask )
+          loss  = self.alphaAE**2 * loss1 + self.alphaObs**2 * loss2
+          #=loss  = loss / ( self.alphaAE**2 + self.alphaObs**2)
+
+          grad = torch.autograd.grad(loss,x,create_graph=True)[0]
+          #grad = torch.autograd.grad(loss,x)[0]
+        
+        elif self.GradType == 3: ## true gradient using autograd for prior ||x-g1(x)||^2 + ||x-g2(x)||^2 
+          if len(self.shape) == 2 :            
+            for ii in range(0,xpred.size(1)):
+               if( ii == 0 ):
+                   loss1 = self.alphaEngTerms[ii]**2 * torch.mean( (xpred[:,ii,:,:].view(-1,self.shape[0],self.shape[1]) - x)**2 )
+               else:
+                   loss1 += self.alphaEngTerms[ii]**2 * torch.mean( (xpred[:,ii,:,:].view(-1,self.shape[0],self.shape[1]) - x)**2 )
+          else:
+               if( ii == 0 ):
+                   loss1 = self.alphaEngTerms[ii]**2 * torch.mean( (xpred[:,0:self.shape[0],:,:] - x)**2 )
+               else:
+                   loss1 += self.alphaEngTerms[ii]**2 * torch.mean( (xpred[:,ii*self.shape[0]:(ii+1)*self.shape[0],:,:] - x)**2 )
+              
+          loss2 = torch.sum( (xobs - x)**2 * mask ) / torch.sum( mask )
+          loss  = self.alphaAE**2 * loss1 + self.alphaObs**2 * loss2
+          #=loss  = loss / ( self.alphaAE**2 + self.alphaObs**2)
+
+          grad = torch.autograd.grad(loss,x,create_graph=True)[0]
+
+        # Check is this is needed or not
         grad.retain_grad()
 
         return grad
@@ -240,7 +286,7 @@ class model_GradUpdate0(torch.nn.Module):
 
         return torch.nn.Sequential(*layers)
 
-    def forward(self, x,xpred,xobs,mask):
+    def forward(self, x,xpred,xobs,mask,gradnorm=1.0):
 
         # compute gradient
         grad = self.compute_Grad(x, xpred,xobs,mask)
@@ -249,7 +295,7 @@ class model_GradUpdate0(torch.nn.Module):
         
         # scaling gradient values
         #grad = grad /self.ScaleGrad
-        grad = grad / torch.sqrt( torch.mean( grad**2 ) )
+        #grad = grad / torch.sqrt( torch.mean( grad**2 ) )
         
         # update
         grad = self.gradNet( grad )
@@ -310,14 +356,15 @@ class model_GradUpdate1(torch.nn.Module):
 
         return torch.nn.Sequential(*layers)
  
-    def forward(self, x,xpred,xobs,mask,grad_old):
+    def forward(self, x,xpred,xobs,mask,grad_old,gradnorm=1.0):
 
         # compute gradient
         grad = self.compute_Grad(x, xpred,xobs,mask)
          
         #grad = grad /self.ScaleGrad
-        grad = grad / torch.sqrt( torch.mean( grad**2 ) )
-
+        #grad = grad / torch.sqrt( torch.mean( grad**2 ) )
+        grad  = grad / gradnorm
+        
         if grad_old is None:
             grad_old = torch.randn(grad.size()).to(device) ## Here device is global variable to be checked
  
@@ -368,9 +415,8 @@ class model_GradUpdate2(torch.nn.Module):
 
         if len(self.shape) == 2: ## 1D Data
             self.lstm = ConvLSTM1d(self.shape[0],self.DimState,3)
-        elif len(self.shape) == 3: ## 2D Data            
+        elif len(self.shape) == 3: ## 2D Data
             self.lstm = ConvLSTM2d(self.shape[0],self.DimState,3)
-
     def _make_ConvGrad(self):
         layers = []
 
@@ -390,14 +436,15 @@ class model_GradUpdate2(torch.nn.Module):
 
         return torch.nn.Sequential(*layers)
  
-    def forward(self, x,xpred,xobs,mask,hidden,cell):
+    def forward(self, x,xpred,xobs,mask,hidden,cell,gradnorm=1.0):
 
         # compute gradient
         grad = self.compute_Grad(x, xpred,xobs,mask)
         
         #grad = grad /self.ScaleGrad
-        grad = grad / torch.sqrt( torch.mean( grad**2 ) )
+        #grad = grad / torch.sqrt( torch.mean( grad**2 ) )
         #grad = self.bn1(grad)
+        grad  = grad / gradnorm
           
         if self.PeriodicBnd == True :
             dB     = 7
@@ -422,6 +469,127 @@ class model_GradUpdate2(torch.nn.Module):
 
         return grad,hidden,cell
 
+class model_GradUpdate3(torch.nn.Module):
+    def __init__(self,ShapeData,GradType,dimState=30):
+        super(model_GradUpdate3, self).__init__()
+
+        with torch.no_grad():
+            self.GradType  = GradType
+            self.shape     = ShapeData
+            self.DimState  = dimState
+            self.layer_dim = 1
+        self.compute_Grad  = Compute_Grad(ShapeData,GradType)
+                
+        if len(self.shape) == 2: ## 1D Data
+            self.convLayer     = torch.Linear(dimState,self.shape[0]*self.shape[1])
+            self.lstm = torch.nn.LSTM(self.shape[0]*self.shape[1],self.DimState,self.layer_dim)
+        else:
+            self.convLayer     = torch.Linear(dimState,self.shape[0]*self.shape[1]*self.shape[2])
+            self.lstm = torch.nn.LSTM(self.shape[0]*self.shape[1]*self.shape[2],self.DimState,self.layer_dim)
+    def forward(self, x,xpred,xobs,mask,hidden,cell,gradnorm=1.0):
+
+        # compute gradient
+        grad = self.compute_Grad(x, xpred,xobs,mask)
+        
+        #grad = grad /self.ScaleGrad
+        #grad = grad / torch.sqrt( torch.mean( grad**2 ) )
+        #grad = self.bn1(grad)
+        grad  = grad / gradnorm
+
+                     
+        if len(self.shape) == 2: ## 1D Data
+            grad = grad.view(-1,1,self.shape[0]*self.shape[1])
+        else:
+            grad = grad.view(-1,1,self.shape[0]*self.shape[1]*self.shape[2])
+        
+        if hidden is None:
+            output,(hidden,cell)  = self.lstm(grad,None)
+        else:
+            output,(hidden,cell) = self.lstm(grad,(hidden,cell))
+
+        grad = self.convLayer( output )
+        if len(self.shape) == 2: ## 1D Data
+            grad = grad.view(-1,self.shape[0],self.shape[1])
+        else:
+            grad = grad.view(-1,self.shape[0],self.shape[1],self.shape[2])
+
+        return grad,hidden,cell
+
+# Gradient-based minimization using a LSTM using a (sub)gradient as inputs
+#class model_GradUpdate2(torch.nn.Module):
+#    def __init__(self,ShapeData,GradType,periodicBnd=False):
+#        super(model_GradUpdate2, self).__init__()
+#
+#        with torch.no_grad():
+#            self.GradType  = GradType
+#            self.shape     = ShapeData
+#            self.DimState  = 5*self.shape[0]
+#            self.PeriodicBnd = periodicBnd
+#            if( (self.PeriodicBnd == True) & (len(self.shape) == 2) ):
+#                print('No periodic boundary available for FxTime (eg, L63) tensors. Forced to False')
+#                self.PeriodicBnd = False
+#        self.compute_Grad  = Compute_Grad(ShapeData,GradType)
+#        self.convLayer     = self._make_ConvGrad()
+#        #self.bn1           = torch.nn.BatchNorm2d(self.shape[0])
+#        #self.lstm            = self._make_LSTMGrad()
+#        K = torch.Tensor([0.1]).view(1,1,1,1)
+#        self.convLayer.weight = torch.nn.Parameter(K)
+#
+#        if len(self.shape) == 2: ## 1D Data
+#            self.lstm = ConvLSTM1d(self.shape[0],self.DimState,3)
+#        elif len(self.shape) == 3: ## 2D Data
+#            self.lstm = ConvLSTM2d(self.shape[0],self.DimState,3)
+#    def _make_ConvGrad(self):
+#        layers = []
+#
+#        if len(self.shape) == 2: ## 1D Data
+#            layers.append(torch.nn.Conv1d(5*self.shape[0], self.shape[0], 1, padding=0,bias=False))
+#        elif len(self.shape) == 3: ## 2D Data            
+#            layers.append(torch.nn.Conv2d(5*self.shape[0], self.shape[0], (1,1), padding=0,bias=False))
+#
+#        return torch.nn.Sequential(*layers)
+#    def _make_LSTMGrad(self):
+#        layers = []
+#
+#        if len(self.shape) == 2: ## 1D Data
+#            layers.append(ConvLSTM1d(self.shape[0],5*self.shape[0],3))
+#        elif len(self.shape) == 3: ## 2D Data            
+#            layers.append(ConvLSTM2d(self.shape[0],5*self.shape[0],3))
+#
+#        return torch.nn.Sequential(*layers)
+# 
+#    def forward(self, x,xpred,xobs,mask,hidden,cell,gradnorm=1.0):
+#
+#        # compute gradient
+#        grad = self.compute_Grad(x, xpred,xobs,mask)
+#        
+#        #grad = grad /self.ScaleGrad
+#        #grad = grad / torch.sqrt( torch.mean( grad**2 ) )
+#        #grad = self.bn1(grad)
+#        grad  = grad / gradnorm
+#          
+#        if self.PeriodicBnd == True :
+#            dB     = 7
+#            #
+#            grad_  = torch.cat((grad[:,:,x.size(2)-dB:,:],grad,grad[:,:,0:dB,:]),dim=2)
+#            if hidden is None:
+#                hidden_,cell_ = self.lstm(grad_,None)
+#            else:
+#                hidden_  = torch.cat((hidden[:,:,x.size(2)-dB:,:],hidden,hidden[:,:,0:dB,:]),dim=2)
+#                cell_    = torch.cat((cell[:,:,x.size(2)-dB:,:],cell,cell[:,:,0:dB,:]),dim=2)
+#                hidden_,cell_ = self.lstm(grad_,[hidden_,cell_])
+#                
+#            hidden = hidden_[:,:,dB:x.size(2)+dB,:]
+#            cell   = cell_[:,:,dB:x.size(2)+dB,:]
+#        else:
+#            if hidden is None:
+#                hidden,cell = self.lstm(grad,None)
+#            else:
+#                hidden,cell = self.lstm(grad,[hidden,cell])
+#
+#        grad = self.convLayer( hidden )
+#
+#        return grad,hidden,cell
 # NN architecture based on given dynamical NN prior (model_AE)
 # solving the reconstruction of the hidden states using a number of 
 # fixed-point iterations prior to a gradient-based minimization
@@ -446,8 +614,10 @@ class Model_4DVarNN_GradFP(torch.nn.Module):
           self.model_Grad = model_GradUpdate1(ShapeData,GradType,self.periodicBnd)
         elif OptimType == 2:
           self.model_Grad = model_GradUpdate2(ShapeData,GradType,self.periodicBnd)
+        elif OptimType == 3:
+          self.model_Grad = model_GradUpdate2(ShapeData,GradType,30)
                 
-    def forward(self, x_inp,xobs,mask,g1=None,g2=None):
+    def forward(self, x_inp,xobs,mask,g1=None,g2=None,normgrad=0.0):
         mask_  = torch.add(1.0,torch.mul(mask,-1.0)) #1. - mask
         
         x      = torch.mul(x_inp,1.0)
@@ -465,26 +635,32 @@ class Model_4DVarNN_GradFP(torch.nn.Module):
 
         # gradient iteration
         if self.NGrad > 0:
+            # gradient normalisation
+            grad     = self.model_Grad.compute_Grad(x, self.model_AE(x),xobs,mask)
+            if normgrad == 0. :
+                _normgrad = torch.sqrt( torch.mean( grad**2 ) )
+            else:
+                _normgrad = normgrad
             for kk in range(0,self.NGrad):
                 # AE pediction
                 xpred = self.model_AE(x)
              
                 # gradient update
                 if self.OptimType == 0:
-                    grad  = self.model_Grad( x, xpred, xobs, mask )
+                    grad  = self.model_Grad( x, xpred, xobs, mask , _normgrad )
         
                 elif self.OptimType == 1:               
                   if kk == 0:
-                    grad  = self.model_Grad( x, xpred, xobs, mask, g1 )
+                    grad  = self.model_Grad( x, xpred, xobs, mask, g1 , _normgrad)
                   else:
-                    grad  = self.model_Grad( x, xpred, xobs, mask,grad_old )
+                    grad  = self.model_Grad( x, xpred, xobs, mask,grad_old , _normgrad)
                   grad_old = torch.mul(1.,grad)
         
                 elif self.OptimType == 2:               
                   if kk == 0:
-                    grad,hidden,cell  = self.model_Grad( x, xpred, xobs, mask, g1, g2 )
+                    grad,hidden,cell  = self.model_Grad( x, xpred, xobs, mask, g1, g2 , _normgrad )
                   else:
-                    grad,hidden,cell  = self.model_Grad( x, xpred, xobs, mask, hidden, cell )
+                    grad,hidden,cell  = self.model_Grad( x, xpred, xobs, mask, hidden, cell , _normgrad )
         
                 # interpolation constraint
                 if( self.InterpFlag == True ):
@@ -496,20 +672,26 @@ class Model_4DVarNN_GradFP(torch.nn.Module):
                     x = x - grad
 
             if self.OptimType == 1:
-                return x,grad_old
+                return x,grad_old,_normgrad
             if self.OptimType == 2:
-                return x,hidden,cell
+                return x,hidden,cell,_normgrad
             else:
-                return x
+                return x,_normgrad
         else:
-            return x
+            _normgrad = 1.
+            if self.OptimType == 1:
+                return x,None,_normgrad
+            if self.OptimType == 2:
+                return x,None,None,_normgrad
+            else:
+                return x,_normgrad
 
 # NN architecture based on given dynamical NN prior (model_AE)
 # solving the reconstruction of the hidden states using fixed-point iterations
 class Model_4DVarNN_FP(torch.nn.Module):
     def __init__(self,mod_AE,ShapeData,NiterProjection):
     #def __init__(self,mod_AE,GradType,OptimType):
-        super(Model_4DVarNN_GradFP, self).__init__()
+        super(Model_4DVarNN_FP, self).__init__()
         self.model_AE = mod_AE
     
         with torch.no_grad():
@@ -548,8 +730,11 @@ class Model_4DVarNN_Grad(torch.nn.Module):
           self.model_Grad = model_GradUpdate1(ShapeData,GradType,self.periodicBnd)
         elif OptimType == 2:
           self.model_Grad = model_GradUpdate2(ShapeData,GradType,self.periodicBnd)
-            
-        
+        elif OptimType == 3:
+          self.model_Grad = model_GradUpdate3(ShapeData,GradType,30)
+        elif OptimType == 4:
+          self.model_Grad = model_GradUpdate4(ShapeData,GradType,self.periodicBnd)
+                    
         with torch.no_grad():
             #print('Opitm type %d'%OptimType)
             self.OptimType = OptimType
@@ -557,7 +742,7 @@ class Model_4DVarNN_Grad(torch.nn.Module):
             self.NGrad     = int(NiterGrad)
             
         
-    def forward(self, x_inp,xobs,mask,g1=None,g2=None):
+    def forward(self, x_inp,xobs,mask,g1=None,g2=None,normgrad=0.):
         if( self.InterpFlag == True ):
             #mask   = torch.add(1.0,torch.mul(mask,0.0)) # set mask to 1 # debug
             mask_  = torch.add(1.0,torch.mul(mask,-1.0)) #1. - mask
@@ -565,25 +750,32 @@ class Model_4DVarNN_Grad(torch.nn.Module):
 
         # gradient iteration
         for kk in range(0,self.NGrad):
+            # gradient normalisation
+            grad     = self.model_Grad.compute_Grad(x, self.model_AE(x),xobs,mask)
+            if normgrad == 0. :
+                _normgrad = torch.sqrt( torch.mean( grad**2 ) )
+            else:
+                _normgrad = normgrad
+
             # AE pediction
             xpred = self.model_AE(x)
          
             # gradient update
             if self.OptimType == 0:
-                grad  = self.model_Grad( x, xpred, xobs, mask )
+                grad  = self.model_Grad( x, xpred, xobs, mask, _normgrad )
     
             elif self.OptimType == 1:               
               if kk == 0:
-                grad  = self.model_Grad( x, xpred, xobs, mask, g1 )
+                grad  = self.model_Grad( x, xpred, xobs, mask, g1, _normgrad )
               else:
-                grad  = self.model_Grad( x, xpred, xobs, mask,grad_old )
+                grad  = self.model_Grad( x, xpred, xobs, mask,grad_old, _normgrad )
               grad_old = torch.mul(1.,grad)
     
             elif self.OptimType == 2:               
               if kk == 0:
-                grad,hidden,cell  = self.model_Grad( x, xpred, xobs, mask, g1, g2 )
+                grad,hidden,cell  = self.model_Grad( x, xpred, xobs, mask, g1, g2, _normgrad )
               else:
-                grad,hidden,cell  = self.model_Grad( x, xpred, xobs, mask, hidden, cell )
+                grad,hidden,cell  = self.model_Grad( x, xpred, xobs, mask, hidden, cell, _normgrad )
     
             # interpolation constraint
             if( self.InterpFlag == True ):
@@ -595,8 +787,8 @@ class Model_4DVarNN_Grad(torch.nn.Module):
                 x = x - grad
 
         if self.OptimType == 1:
-            return x,grad_old
+            return x,grad_old,_normgrad
         if self.OptimType == 2:
-            return x,hidden,cell
+            return x,hidden,cell,_normgrad
         else:
-            return x
+            return x,_normgrad
